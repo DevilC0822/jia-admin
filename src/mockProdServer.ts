b154705699a929fafda331e936947b85839dd23a
DevilC <devilc0822@gmail.com>
2023-03-07 11:20:27 +0800

    test: vite-plugin-mock在服务器上使用

--- src/mockProdServer.ts
+++ src/mockProdServer.ts
@@ -1,13 +1,11 @@
 import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer';
-const modules = import.meta.glob('./service/mock/*.ts');
 
+const modules: any = import.meta.glob('./service/mock/*.ts', { eager: true });
 const mockModules: any[] = [];
-for (const path in modules) {
-  modules[path]().then((mod: any) => {
-    if (mod.default) {
-      mockModules.push(...mod.default);
-    }
-  });
+for (const item in modules) {
+  if (modules[item].default) {
+    mockModules.push(...modules[item].default);
+  }
 }
 
 export function setupProdMockServer() {
